12c4c3b183b8d2b6a476adc78a549c08fa4e937f
jamisongrant <jgbatchelor@gmail.com>
2017-03-02 14:57:20 -0700

    building component to create new proposals

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
